b44a62ea5c29ee1fc8e2a3f1a7de351b1f868fde
Karina <puentek@oregonstate.edu>
2022-03-09 21:12:03 -0600

    motion detection with frames

--- ArmPi/Functions/cam_detect.py
+++ ArmPi/Functions/cam_detect.py
@@ -99,7 +99,7 @@ def no_motion(my_camera):
                 frame_new = get_mask(f)
                 frame_final = (frame_new/255)-(frame_i/255)
                 v = np.sum(np.abs(frame_final))
-                print("value:", v, frame_final , np.max(frame_new))
+                print("value:", v)
 
                 if v < threshold:
                     status = True
